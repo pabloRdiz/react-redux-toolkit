16aaab0c7101be24af2efe9332245aca53e8badb
pabloRdiz <pablorodriguez833@gmail.com>
2023-04-15 11:18:56 -0300

    configure romes rules and applying theirs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { ListOfUsers } from "./components/ListOfUsers";
 function App() {
-	return (
-		<>
-			<h1>React Redux Toolkit</h1>
-			<ListOfUsers />
-		</>
-	);
+  return (
+    <>
+      <h1>React Redux Toolkit</h1>
+      <ListOfUsers />
+    </>
+  );
 }
 
 export default App;
